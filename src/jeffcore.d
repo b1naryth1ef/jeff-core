264710e7ac2da0c00eac2762d36de44122bd296a
Andrei <b1naryth1ef@gmail.com>
2016-08-25 04:07:16 -0500

    Add eval command :^)

--- src/jeffcore.d
+++ src/jeffcore.d
@@ -1,8 +1,16 @@
 module jeffcore;
 
-import std.array;
+import std.array,
+       std.format,
+       std.path,
+       std.stdio,
+       std.file,
+       std.algorithm;
+
 import dscord.core,
-       dscord.util.emitter;
+       dscord.util.process,
+       dscord.util.emitter,
+       dscord.util.dynlib;
 
 class CorePlugin : Plugin {
   this() {
@@ -11,7 +19,7 @@ class CorePlugin : Plugin {
 
   @Command("ping")
   void onPing(CommandEvent event) {
-    event.msg.reply("pong");
+    event.msg.replyf("pong");
   }
 
   Plugin pluginCommand(CommandEvent e, string action) {
@@ -92,7 +100,63 @@ class CorePlugin : Plugin {
     }
     e.msg.reply("Saved all storage!");
   }
+
+  @Command("eval")
+  @CommandDescription("evaluate code")
+  @CommandLevel(Level.ADMIN)
+  void onEval(CommandEvent e) {
+    string contents = find(e.msg.content, "eval ")[5..$];
+
+    if (contents.startsWith("```") && contents.endsWith("```")) {
+      contents = contents[3..$-3];
+    } else if (contents.startsWith("`") && contents.endsWith("`")) {
+      contents = contents[1..$-1];
+    }
+
+    contents = contents.split("\n").map!((x) => x.length == 0 || x.endsWith(";") ? x : x ~ ";").array.join("\n");
+
+    string libpath = this.storageDirectoryPath ~ dirSeparator ~ "eval";
+
+    auto fo = File(libpath ~ dirSeparator ~ "src" ~ dirSeparator ~ "eval.d", "w");
+    fo.writef(EVAL_TEMPLATE, contents);
+    fo.close();
+
+    string cwd = getcwd();
+    chdir(libpath);
+    Process p = Process(["dub", "build"]);
+    chdir(cwd);
+
+    if (p.wait() != 0) {
+      MessageBuffer buf = new MessageBuffer(true);
+
+      string data;
+      while ((data = p.stderr.readln()) !is null) {
+        buf.append(data);
+      }
+
+      e.msg.reply(buf);
+      return;
+    }
+
+    auto lib = loadDynamicLibrary(libpath ~ dirSeparator ~ "libeval.so");
+    auto f = lib.loadFromDynamicLibrary!evalFunction("eval");
+    f(this, e);
+    unloadDynamicLibrary(lib);
+  }
 }
+
+alias evalFunction = void function(Plugin, CommandEvent);
+
+immutable EVAL_TEMPLATE = `
+module eval;
+
+import dscord.core;
+
+// Args are flipped because !?!?
+extern (C) void eval(CommandEvent e, Plugin p) {
+  %s
+}
+`;
 
 extern (C) Plugin create() {
   return new CorePlugin;
